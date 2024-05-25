e284c650f512c954d88868a31687e2d1e5b2b625
Félix Courtin <felix.courtin@proton.me>
2024-05-25 14:19:40 +0200

    Initial model

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import ast
 from sklearn.model_selection import train_test_split
 from langchain.prompts import (
     ChatPromptTemplate,
@@ -10,13 +11,34 @@ from langchain.prompts import (
 from langchain.chains import ConversationChain
 from langchain_openai import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
-
+import re
 import utils
+import xgboost as xgb
+from sklearn.experimental import enable_halving_search_cv
+from sklearn.model_selection import HalvingRandomSearchCV
+from scipy.stats import randint, uniform
 
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Data uploading", "Data characteristics", "Metrics suggestions", "Initial model", "Refine model" "Export pipeline"])
+page = st.sidebar.radio("Go to", ["Data uploading", "Data characteristics", "Metrics suggestions", "Initial model", "Refine model"])
 
 st.title('LLM assisted AutoML app')
+openai_api_key = '"YOUR API KEY HERE"'
+llm = ChatOpenAI(temperature=0.6, openai_api_key=openai_api_key)
+memory = ConversationBufferMemory(return_messages=True)
+st.session_state.last_run_best_score = []
+st.session_state.all_time_best_score = []
+system_message = f"""
+    You are a senior data scientist tasked with guiding the use of an AutoML tool  
+    to discover the best XGBoost model configurations for a given binary classification dataset. 
+    Your role involves understanding the dataset characteristics, proposing suitable metrics, 
+    hyperparameters, and their search spaces, analyzing results, and iterating on configurations. 
+    """
+prompt = ChatPromptTemplate.from_messages([
+    SystemMessagePromptTemplate.from_template(system_message),
+    MessagesPlaceholder(variable_name="history"),
+    HumanMessagePromptTemplate.from_template("""{input}""")
+])
+conversation = ConversationChain(memory=memory, prompt=prompt, llm=llm, verbose=False)
 
 if page == "Data uploading":
     st.write("""#### Upload your data""")
@@ -32,40 +54,51 @@ if page == "Data uploading":
 
             if st.session_state.df is not None:
                 st.write(st.session_state.df)
-                st.write("Shape of the dataset:", st.session_state.df.shape)
-                st.write("Summary statistics:", st.session_state.df.describe())
+                st.session_state.user_input = st.text_area("Please, briefly describe your data:", "")
 
         except Exception as e:
             st.error(f"Error loading file: {e}")
 
 if page == "Data characteristics":
-    st.write("""#### Data Characteristics""")
-    st.header("Data report")
-    st.session_state.report, st.session_state.target = utils.data_report(st.session_state.df)
+    st.header("Data Characteristics")
+    st.write("""#### Data Report""")
+    st.session_state.df, st.session_state.report, st.session_state.target = utils.data_report(st.session_state.df)
     st.write(st.session_state.report)
 
 if page == "Metrics suggestions":
-    st.write("""#### Metrics suggestions""")
-    st.session_state.X_train, st.session_state.X_test, st.session_state.y_train, st.session_state.y_test = train_test_split(st.session_state.df, st.session_state.target, test_size=0.2, random_state=42)
-    openai_api_key = '"YOUR_OPENAI_API_KEY"'
-    llm = ChatOpenAI(temperature=0.6, openai_api_key=openai_api_key)
-    memory = ConversationBufferMemory(return_messages=True)
-    st.session_state.last_run_best_score = []
-    st.session_state.all_time_best_score = []
-    system_message = f"""
-    You are a senior data scientist tasked with guiding the use of an AutoML tool  
-    to discover the best XGBoost model configurations for a given binary classification dataset. 
-    Your role involves understanding the dataset characteristics, proposing suitable metrics, 
-    hyperparameters, and their search spaces, analyzing results, and iterating on configurations. 
-    """
-    prompt = ChatPromptTemplate.from_messages([
-        SystemMessagePromptTemplate.from_template(system_message),
-        MessagesPlaceholder(variable_name="history"),
-        HumanMessagePromptTemplate.from_template("""{input}""")
-    ])
-    conversation = ConversationChain(memory=memory, prompt=prompt, llm=llm, verbose=False)
+    st.header("Metrics suggestions")
 
-    prompt = utils.suggest_metrics(st.session_state.report)
+    st.session_state.X_train, st.session_state.X_test, st.session_state.y_train, st.session_state.y_test = train_test_split(st.session_state.df, st.session_state.target, test_size=0.2, random_state=42)
+
+    prompt = utils.suggest_metrics(st.session_state.user_input, st.session_state.report)
     response = conversation.predict(input=prompt)
-    st.header("GPT Chat")
+    st.write("""#### GPT Chat""")
     st.write(response)
+    st.session_state.metric = utils.extract_metric(response)
+
+if page == "Initial model":
+
+    st.header("Initial Model")
+    prompt = utils.suggest_initial_search_space()
+    response = conversation.predict(input=prompt)
+    st.write(response)
+    search_space_string = utils.extract_search_space(response)
+    search_space_dict = {}
+    if search_space_string:
+        from scipy.stats import randint, uniform
+        exec(search_space_string, globals(), search_space_dict)
+        search_space = search_space_dict.get('search_space', {})
+        clf = xgb.XGBClassifier(seed=42, objective='binary:logistic', enable_categorical=True, eval_metric=st.session_state.metric, use_label_encoder=False)
+
+        search = HalvingRandomSearchCV(clf, search_space, scoring=st.session_state.metric, n_candidates=50,
+                                       cv=5, min_resources='exhaust', factor=3, verbose=1).fit(st.session_state.X_train, st.session_state.y_train)
+
+        y_pred = search.predict(st.session_state.X_test)
+        y_pred_proba = search.predict_proba(st.session_state.X_test)
+        utils.metrics_display(st.session_state.y_test, y_pred, y_pred_proba[:, 1])
+    else:
+        st.error("No search space found for initial model")
+
+
+if page == "Refine model":
+    st.header("Refine Model")
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import streamlit as st
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.metrics import (
     precision_score,
@@ -8,23 +9,19 @@ from sklearn.metrics import (
     f1_score,
     matthews_corrcoef
 )
+import re
 
 
 def metrics_display(y_test, y_pred, y_pred_proba):
     cm = confusion_matrix(y_test, y_pred)
-
     tn, fp, fn, tp = cm.ravel()
-
-    print(f'ROC_AUC score: {roc_auc_score(y_test, y_pred_proba):.3f}')
-    print(f'f1 score: {f1_score(y_test, y_pred):.3f}')
-    print(f'Accuracy: {accuracy_score(y_test, y_pred) * 100:.2f}%')
-    print(f'Precision: {precision_score(y_test, y_pred) * 100:.2f}%')
-    print(f'Detection rate: {recall_score(y_test, y_pred) * 100:.2f}%')
-    print(f'False alarm rate: {fp / (tn + fp) * 100}%')
-    print(f'MCC: {matthews_corrcoef(y_test, y_pred):.2f}')
-
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-    disp.plot()
+    st.write(f'ROC_AUC score: {roc_auc_score(y_test, y_pred_proba):.3f}')
+    st.write(f'f1 score: {f1_score(y_test, y_pred):.3f}')
+    st.write(f'Accuracy: {accuracy_score(y_test, y_pred) * 100:.2f}%')
+    st.write(f'Precision: {precision_score(y_test, y_pred) * 100:.2f}%')
+    st.write(f'True positive rate: {recall_score(y_test, y_pred) * 100:.2f}%')
+    st.write(f'False positive rate: {fp / (tn + fp) * 100}%')
+    st.write(f'MCC: {matthews_corrcoef(y_test, y_pred):.2f}')
 
 
 def data_report(df):
@@ -56,23 +53,27 @@ def data_report(df):
             str_counts += 1
             str_list.append(feature)
 
+    for col in str_list:
+        df[col] = df[col].astype('category')
+
     class_counts = target.value_counts()
     class_distribution = class_counts / num_instances
     if any(class_distribution < 0.3) or any(class_distribution > 0.7):
         class_imbalance = True
     else:
         class_imbalance = False
+    df_without_target = df.drop(df.columns[-1], axis=1)
+    report = f"""
+    Data characteristics report :
 
-    report = f"""Data characteristics report :
-        - General information :
-            - Number of instances : {num_instances}
-            - Number of features : {num_features}
-        
-        - Class Distribution analysis :
-            - Class distribution : {class_distribution.to_string()}
-        {'Warning : Class imbalance detected' if class_imbalance else 'No class imbalance detected'}
-    
-        - Feature analysis :
+        General information :
+        - Number of instances : {num_instances}
+        - Number of features : {num_features}
+
+    Class Distribution analysis :
+        Class distribution : {class_distribution.to_string()}\n{'Class imbalance detected' if class_imbalance else 'No class imbalance detected'}
+
+        Feature analysis :
             - Feature names : {features.columns.to_list()}
             - Number of binary features : {binary_counts}
             - Binary features : {binary_list}
@@ -81,22 +82,20 @@ def data_report(df):
             - Number of string features : {str_counts}
             - String features : {str_list}
             - Number of integer features : {int_counts}
-            - Integer features : {int_list}      
+            - Integer features : {int_list}
     """
-    return report, target
+    return df_without_target, report, target
 
 
-def suggest_metrics(report):
+def suggest_metrics(description, report):
     prompt = f"""
-    The classification problem under investigation is based on a network intrusion detection dataset. 
-    This dataset contains DOS, Probe, R2L, and U2R attack types, which are all grouped under the 
-    "attack" class (label: 1). Conversely, the "normal" class is represented by label 0. 
+    Here is a brief description of the dataset :
+    {description}
     Below are the dataset's characteristics:
     {report}.
 
     For this specific inquiry, you are tasked with recommending a suitable hyperparameter optimization 
-    metric for training a XGBoost model. It is crucial that the model should accurately identify genuine 
-    threats (attacks) without raising excessive false alarms on benign activities. They are equally important.
+    metric for training a XGBoost model.
     Given the problem context and dataset characteristics, suggest only the name of one of the built-in 
     metrics: 
     - 'accuracy'
@@ -117,3 +116,57 @@ def suggest_metrics(report):
     """
 
     return prompt
+
+
+def suggest_initial_search_space():
+    prompt = f"""
+    Given your understanding of XGBoost and general best practices in machine learning, suggest an 
+    initial search space for hyperparameters. 
+
+    Tunable hyperparameters include:
+    - n_estimators (integer): Number of boosting rounds or trees to be trained.
+    - max_depth (integer): Maximum tree depth for base learners.
+    - min_child_weight (integer or float): Minimum sum of instance weight (hessian) needed in a leaf node. 
+    - gamma (float): Minimum loss reduction required to make a further partition on a leaf node of the tree.
+    - scale_pos_weight (float): Balancing of positive and negative weights.
+    - learning_rate (float): Step size shrinkage used during each boosting round to prevent overfitting. 
+    - subsample (float): Fraction of the training data sampled to train each tree. 
+    - colsample_bylevel (float): Fraction of features that can be randomly sampled for building each level (or depth) of the tree.
+    - colsample_bytree (float): Fraction of features that can be randomly sampled for building each tree. 
+    - reg_alpha (float): L1 regularization term on weights. 
+    - reg_lambda (float): L2 regularization term on weights. 
+
+    The search space is defined as a dict with keys being hyperparameter names, and values 
+    are the search space associated with the hyperparameter. For example:
+        search_space = {{
+            "learning_rate": loguniform(1e-4, 1e-3)
+        }}
+
+    Available types of domains include: 
+    - scipy.stats.uniform(loc, scale), it samples values uniformly between loc and loc + scale.
+    - scipy.stats.loguniform(a, b), it samples values between a and b in a logarithmic scale.
+    - scipy.stats.randint(low, high), it samples integers uniformly between low (inclusive) and high (exclusive).
+    - a list of possible discrete value, e.g., ["a", "b", "c"]
+
+    Please first briefly explain your reasoning, then provide the configurations of the initial 
+    search space. Enclose your suggested configurations between markers 
+    [BEGIN] and [END], and assign your configuration to a variable named search_space.
+    """
+
+    return prompt
+
+
+def extract_search_space(text):
+    pattern = re.compile(r'search_space\s*=\s*{[^}]*}', re.DOTALL)
+    match = pattern.search(text)
+    if match:
+        return match.group(0)
+    return None
+
+
+def extract_metric(text):
+    pattern = re.compile(r'\[BEGIN] *\'(.*?)\' *\[END]')
+    match = pattern.search(text)
+    if match:
+        return match.group(0)
+    return None
